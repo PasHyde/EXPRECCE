eacdb961f2b41c3f5d3aadd6586c083a2fd70e09
PasHyde <79587588+PasHyde@users.noreply.github.com>
2023-12-04 16:37:53 +0200

    Update co_occurrence_network.py

--- co_occurrence_network.py
+++ co_occurrence_network.py
@@ -1,7 +1,7 @@
 '''
 This Python module automatically generates a word co-occurrence network using a plain text file as input. 
 A user specifies the target words (terms under study) and the window size, i.e., the number of words considered before and after the 
-target words. Then, a network is drawn using a spring layout. This algorithm simulates a force-directed representation of the network treating edges as springs 
+target words. Then, a co-occurrence network is drawn using a spring layout. This algorithm simulates a force-directed representation of the network treating edges as springs 
 holding nodes close while treating nodes as repelling objects.
 '''
 
